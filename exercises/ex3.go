2415243ec5a4efec69ca5c42d2765fbdcc7bc091
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-02-08 00:31:57 -0300

    feat: complete ex3

--- exercises/ex3.go
+++ exercises/ex3.go
@@ -1,11 +1,15 @@
 package exercises
 
-func isPrime(n int) bool {
-	if n <= 2 { // 0, 1 and 2 are not prime numbers
+import "fmt"
+
+func isPrime(n int64) bool {
+	if n < 2 { // 0, 1 and 2 are not prime numbers
 		return false
 	}
 
-	for i := 2; i < n/2; i++ {
+	var i int64
+
+	for i = 2; i <= n/2; i++ {
 		if n%i == 0 {
 			// if rest of division is equal to zero, it means that the number has
 			// a divider that's not 1 and itself
@@ -17,4 +21,15 @@ func isPrime(n int) bool {
 }
 
 func LargestPrimeFactor() {
+	fixedN := 20
+	n := 20
+	i := 2
+
+	for i < n {
+		for n%i == 0 {
+			n = n / i
+		}
+		i++
+	}
+	fmt.Printf("The largest prime factor for %d is: %d \n", fixedN, i)
 }
